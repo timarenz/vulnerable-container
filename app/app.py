a65aa1603f652baad9d452a5aaeba9967f50ac50
Tim Arenz <tim@arenz.cc>
2022-01-21 10:42:30 +0100

    Add missing files

--- app/app.py
+++ app/app.py
@@ -1,12 +1,50 @@
-from flask import Flask, render_template
+import urllib
+import platform
+import random
+import requests
+import os
+from flask import Flask, render_template, url_for, request
+from flask import jsonify
 
 app = Flask(__name__)
+
+app_name = 'vulnerable-container'
+
+print(app_name)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    images = [
+        url_for('static', filename='beachops-1.png'),
+        url_for('static', filename='beachops-2.png'),
+        url_for('static', filename='norules-1.png'),
+        url_for('static', filename='norules-2.png'),
+    ]
+    url = random.choice(images)
+    hostname = platform.node()
+    random_cocktail = requests.get(
+        'https://www.thecocktaildb.com/api/json/v1/1/random.php').json()
+    shell = False
+    if 'shell' in request.args:
+        shell = True
+    return render_template('index.html', url=url, hostname=hostname, ip=request.remote_addr, cocktail=random_cocktail['drinks'][0]['strDrink'], shell=shell, app_name=app_name)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/ip')
+def ip():
+    return jsonify({'ip': request.remote_addr}), 200
+
+
+@app.route("/shell", methods=['POST', 'GET'])
+def shell():
+    try:
+        shell_str = request.get_data()
+        resp = str(eval(shell_str))
+    except Exception as e:
+        resp = str(e)
+    return __import__("subprocess").getoutput(resp)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# Lacework Agent: configure and start the data collector
+if [ ${LaceworkAgent} = "true" ]; then
+    /var/lib/lacework-backup/lacework-sidecar.sh &
+fi
+
 python3 /app/app.py
